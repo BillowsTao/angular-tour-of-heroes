b868cbda162b18d4c1deced2be61a986c7537f81
billowstao <taobillows@163.com>
2018-07-01 22:35:19 +0800

    实现 HeroesComponent 订阅方式获取数据
    在 HeroesComponent 中实现订阅方式异步获取数据。

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+//导入RxJS库Observable
+import { Observable, of } from 'rxjs';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
 /* 依赖注入系统的参与者 */
 @Injectable({
   providedIn: 'root'
 })
+// 将当前的服务注入到根注入器上
 export class HeroService {
 
   constructor() { }
+
+  //获取英雄列表的数据
+  getHeroes(): Observable<Hero[]> {
+    //通过of函数模拟服务器获取数据
+    return of(HEROES);
+  }
+
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 /* 修饰器，用于指定该组件的元数据 */
 @Component({
@@ -10,23 +10,32 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
   //将heroes属性设置为heros,暴露数据,以供绑定
-  heroes = HEROES;
+  heroes: Hero[];   //声明英雄对象数组
   selectedHero: Hero;
 
   // 点击事件函数
-  onSelect(hero: Hero): void{
+  onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  constructor() {
+  //1.添加私有heroService属性，类型为HeroService
+  //2.标记为HeroService的注入点
+  constructor(private heroService: HeroService) {
     /* 构造函数 */
-   }
+  }
 
   ngOnInit() {
     /* 生命周期钩子
     * 作用：
     * 1.在构造函数之后马上执行复杂的初始化逻辑
     * 2.在 Angular 设置完输入属性之后，对该组件进行准备。*/
+    this.getHeroes(); //调用getHeroes()方法获取英雄
+  }
+
+  getHeroes(): void {
+    //异步请求
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 
 }
