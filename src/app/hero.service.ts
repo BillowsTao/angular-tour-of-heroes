90c29e8abe6e8b52b6ede6b94b58f9e05c246b3e
billowstao <taobillows@163.com>
2018-07-02 16:55:52 +0800

    完成异常捕捉
    完成异常捕捉处理handler函数

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,32 +1,78 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 //导入RxJS库Observable
+import { catchError, map, tap } from 'rxjs/operators';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
 /* 依赖注入系统的参与者 */
 @Injectable({
   providedIn: 'root'
 })
-// 将当前的服务注入到根注入器上
+/* 将当前的服务注入到根注入器上 */
 export class HeroService {
+  private heroesUrl = 'api/heroes';
+  constructor(
+    private http: HttpClient, //将 HttpClient 注入到名叫http的私有属性中
+    private messageService: MessageService) { }
 
-  constructor(private messageService: MessageService) { }
+  /* 日志函数 */
+  private log(message: string) {
+    this.messageService.add('HeroService: ' + message);
+  }
 
-  //获取英雄列表的数据
+  /* 获取英雄列表的数据 */
   getHeroes(): Observable<Hero[]> {
-    //通过of函数模拟服务器获取数据
-    //获取到英雄信息时发送一条消息
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    /* 调用HttpClient的方法获取数据 */
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(heroes => this.log(`fetched heroes`)),
+        catchError(this.handleError('getHeroes', []))
+      );
+  }
+
+  /* 异常处理函数 */
+  /**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 
   //通过id获取英雄
   getHero(id: number): Observable<Hero> {
-    // TODO: send the message _after_ getching the hero
     // 通过``提供模板字符串环境
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
 
+  /* 更新英雄信息 */
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
+  }
 }
